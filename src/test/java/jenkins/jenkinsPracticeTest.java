de76aa6526c62f9737d074600b47c51607b528cb
Radhika B <Radhika B@FFE-Radhika-B>
2025-11-04 15:38:58 +0530

    first commit

---
+++ src/test/java/jenkins/jenkinsPracticeTest.java
@@ -0,0 +1,9 @@
+package jenkins;
+
+public class jenkinsPracticeTest {
+	public void jenkins()
+	{
+		System.out.println("hii Jenkins");
+	}
+
+}
---
+++ src/test/java/maven/MavenPracticeTest.java
@@ -0,0 +1,9 @@
+package maven;
+
+public class MavenPracticeTest {
+	public void maven()
+	{
+		System.out.println("hiii maven");
+	}
+
+}
